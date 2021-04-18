09f1b469a27bd52c95d734420fa00055be9219f0
t-bendet <talbendet21@gmail.com>
2021-04-18 15:24:35 +0300

    updating

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const bodyParser = require("body-parser");
 const { getMoviesList, addMovie, getMovie, deleteMovie } = require("./utils");
 const app = express();
 const cors = require("cors");
